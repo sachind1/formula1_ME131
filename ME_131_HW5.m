259063e218a0fe67f2bda23229741d395e95f796
Sachin DeYoung <sachindeyoung@gmail.com>
2018-03-16 19:55:38 -0700

    updated HW5

--- ME_131_HW5.m
+++ ME_131_HW5.m
@@ -1,10 +1,20 @@
 %% problem 2
-% load data
-rho = 1.225; % kg/m3
-m = 1755; % kg
+% 2.1
+load('straightLineTest.mat')
+%rho = 1.225; % kg/m3
+%m = 1755; % kg
 g = 9.81; % m/s2
 A_f = 1.6 + 0.00056*(m - 765); % m2
-%V_o = Vx(0); % m/s
 
+C_d = 
+R_x = 
+beta = Vx(1) * (rho*A_f*C_d/2/R_x)^1/2; % V initial = Vx(1)
 
-TEST
+vt_over_Vo = [];
+T = t(end); % assuming given data goes until car effectively stops
+for idx = 1:numel(t)
+    vt_over_Vo = [vt_over_Vo (1/beta)*tan( (1-t(idx)/T) * atan(beta) )];
+end
+t_over_T = t./T;
+plot(t_over_T, vt_over_Vo)
+
