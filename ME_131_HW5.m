0312119fee7f3bb6c1158a1bc2224ac711c8fd5c
Sachin DeYoung <sachindeyoung@gmail.com>
2018-03-16 20:57:44 -0700

    updated HW5

--- ME_131_HW5.m
+++ ME_131_HW5.m
@@ -1,20 +1,27 @@
-%% problem 2
-% 2.1
+%% 2.1
 load('straightLineTest.mat')
 %rho = 1.225; % kg/m3
 %m = 1755; % kg
 g = 9.81; % m/s2
 A_f = 1.6 + 0.00056*(m - 765); % m2
-
-C_d = 
-R_x = 
-beta = Vx(1) * (rho*A_f*C_d/2/R_x)^1/2; % V initial = Vx(1)
-
-vt_over_Vo = [];
+V_o = Vx(1);
 T = t(end); % assuming given data goes until car effectively stops
-for idx = 1:numel(t)
-    vt_over_Vo = [vt_over_Vo (1/beta)*tan( (1-t(idx)/T) * atan(beta) )];
-end
+
+vt_over_Vo = Vx./V_o;
 t_over_T = t./T;
 plot(t_over_T, vt_over_Vo)
+%% 2.2
+% solve for beta, then get C_d and R_x
+%beta = V_o * (rho*A_f*C_d/2/R_x)^1/2;
+
+% lsqcurvefit
+func_out = @(beta, t_over_T) (1/beta)*tan( (1-t_over_T) * atan(beta) );
+x0 = [1]; % don't think value matters?, numel = 1 b/c only one variable (beta)
+beta = lsqcurvefit(func_out, x0, t_over_T, vt_over_Vo)
+
+% fminunc 
+
+% C_d = (2*m*beta*atan(beta)) / (V_o*T*rho*A_f);
+% R_x = (V_o*m*atan(beta)) / (beta*T);
+
 
