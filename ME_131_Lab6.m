3c7f6c944f84a3e90f23c5b78b38816a95526583
Sachin DeYoung <sachindeyoung@gmail.com>
2018-03-22 20:04:36 -0700

    Lab 6 work

--- ME_131_Lab6.m
+++ ME_131_Lab6.m
@@ -1,1 +1,8 @@
 %% init
+m = 5; % kg
+g = 9.8; % m/s2
+rho = 1.225; % kg/m3
+A_f = 0.15 * 0.25 * 0.9; % m2, provided by NSCEP empirical formula
+V_init = 0.5; %?
+R_x = 2; %PLACEHOLDER
+C_d = 14; %PLACEHOLDER
